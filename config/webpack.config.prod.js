528469f53cd1a414d116531d0e2de5b689bf2102
tanliyuan <tanliyuan@wdai.com>
2017-10-25 12:50:11 +0800

    优化，增加离线监测

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -133,6 +133,12 @@ module.exports = {
           },
         ],
         include: paths.appSrc,
+          exclude: paths.appLib,
+      },
+      {
+            test: /offline\.min\.js$/,
+            include: paths.appLib,
+            loader: require.resolve('script-loader'),
       },
       // ** ADDING/UPDATING LOADERS **
       // The "file" loader handles all assets unless explicitly excluded.
@@ -173,6 +179,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         include: paths.appSrc,
+          exclude: paths.appLib,
         loader: require.resolve('babel-loader'),
         options: {
             plugins: [
@@ -367,6 +374,9 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.ProvidePlugin({
+          Offline: './lib/offline.min',
+    }),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
